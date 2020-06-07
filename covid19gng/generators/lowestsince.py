b9b1422d36fa71e583c4ba0802d3fd283bd688a2
John Luscombe <jluscombe96@gmail.com>
2020-06-06 22:26:00 -0400

    Redid 'lowestsince' logic

--- covid19gng/generators/lowestsince.py
+++ covid19gng/generators/lowestsince.py
@@ -7,13 +7,21 @@ recoveries or deaths since a particular date.
 """
 
 from datetime import datetime
+from datetime import timedelta
 
+from covid19gng.constants import EARLIEST
 from covid19gng.constants import DF_DATE_FORMAT
 from covid19gng.constants import REPORT_DATE_FORMAT
 from covid19gng.generators import CountryAndStateGenerator
 
-# Minimum number of days the previous date must be in order to get reported
-DAYS_THRESHOLD = 30
+# Latest the previous date must be in order to get reported
+DATE_THRESHOLD = datetime.now() - timedelta(days=30)
+
+# Earliest date as datetime object
+EARLIEST = datetime.strptime(EARLIEST, DF_DATE_FORMAT)
+
+# Earliest date as formatted object
+EARLIEST_FORMATTED = EARLIEST.strftime(REPORT_DATE_FORMAT)
 
 
 class LowestSinceGenerator(CountryAndStateGenerator):
@@ -34,26 +42,50 @@ class LowestSinceGenerator(CountryAndStateGenerator):
         series = (series - series.shift(1))[1:]
 
         todays_value = series[-1]
-        previous_date = None
 
-        i = 1
-        for date in series.index[-2::-1]:
-            value = series[date]
+        # Take out today's value from series
+        series = series[:-1]
 
-            if value <= todays_value:
-                if i >= DAYS_THRESHOLD and value >= 0:
-                    previous_date = date
-                break
+        candidates = series[series <= todays_value]
 
-            i += 1
+        if len(candidates) == 0:
+            # Today's value is lowest in recorded history!
+            print(f"* **{location}** \\- lowest {self._data_desc} "
+                  f"since before {EARLIEST_FORMATTED} ({int(todays_value):,})")
 
-        if previous_date is not None:
+            return True
 
-            previous_date = datetime.strptime(previous_date, DF_DATE_FORMAT)
+        previous_date_str = candidates.index[-1]
+        previous_date = datetime.strptime(previous_date_str, DF_DATE_FORMAT)
+        previous_value = candidates[-1]
+
+        earliest_date = None
+
+        if previous_value <= 0:
+            # Any value less than or equal to 0 may not be reliable, so look
+            # for a potential earlier date
+            earlier_candidates = candidates[candidates > 0]
+
+            if len(earlier_candidates) > 0:
+                earliest_date_str = earlier_candidates.index[-1]
+                earliest_date = datetime.strptime(earliest_date_str, DF_DATE_FORMAT)
+
+        do_report = (previous_date is not None and
+                     previous_date <= DATE_THRESHOLD) or \
+                    (earliest_date is not None and
+                     earliest_date <= DATE_THRESHOLD)
+
+        if do_report:
             previous_date = previous_date.strftime(REPORT_DATE_FORMAT)
 
-            print(f"* **{location}** \\- lowest {self._data_desc} "
-                  f"since {previous_date} ({int(todays_value):,})")
+            report = f"* **{location}** \\- lowest {self._data_desc} " \
+                     f"since {previous_date} ({int(todays_value):,})"
+
+            if earliest_date is not None:
+                earliest_date = earliest_date.strftime(REPORT_DATE_FORMAT)
+                report += f" (possibly as early as {earliest_date})"
+
+            print(report)
 
             return True
 
